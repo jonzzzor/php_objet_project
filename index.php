7883cf16aa8be748bb4679d2995e866822211b9f
Jonathan <39195837+jonzzzor@users.noreply.github.com>
2019-02-15 12:34:35 +0100

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-spl_autoload_register(function ($class_name) {
-    $path_class = './class'.$class_name.'.class.php';
-    // echo $path;
-    require $path_class;
-});
-
-// header("Content-type: application/xhtml+xml");
