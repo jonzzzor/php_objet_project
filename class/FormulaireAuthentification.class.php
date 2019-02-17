974224eef1b842459a8fe1480f7cd6750537800d
christophe <christophe.couronne@yahoo.fr>
2019-02-17 15:38:24 +0100

    add buttons Accueil and Depot, checked or unchecked on navbar

--- class/FormulaireAuthentification.class.php
+++ class/FormulaireAuthentification.class.php
@@ -12,19 +12,40 @@ class FormulaireAuthentification
         $button_connect_disconnect ="";
         if (Authentification::getInstance()->isAuth()) { 
 
-            //echo "<p>Bienvenue ". SESSION::getInstance()->getUserNom()."</p>"; 
+            //echo "<p>Bienvenue ". SESSION::getInstance()->getUserNom()."</p>";
+            
+            $accueil = "<div class='col-md-3'>
+            <a href='index.php'><input class='btn btn-secondary' type='button' value='Accueil'></a>       
+            </div>
+            <div class='col-md-3'>
+            <a href='depot.php'><input class='btn btn-outline-secondary' type='button' value='Dépôt'></a>                             
+            </div>";
+            $depot = "<div class='col-md-3'>
+            <a href='index.php'><input class='btn btn-outline-secondary' type='button' value='Accueil'></a>       
+            </div>
+            <div class='col-md-3'>
+            <a href='depot.php'><input class='btn btn-secondary' type='button' value='Dépôt'></a>                             
+            </div>";
+            $adress = $_SERVER['PHP_SELF'];
+            preg_match('/(.+)\/(.+\.php)$/', $adress, $page);
             echo " 
             <div class='container navbar_perso'>
                 <nav class='navbar navbar-light bg-dark'>
                     <p class='text_welcome_user'>Bienvenue ". SESSION::getInstance()->getUserNom()."</p>
-                    <form action='#' class='form-inline ml-auto' method='POST'>
-                        <div class='col-md-4'>
+                    <form action='#' class='form-inline ml-auto' method='POST'>";
+                        if($page[2] == 'index.php'){
+                            echo $accueil;
+                        }else{
+                            echo $depot;
+                        }
+                        echo "<div class='col-md-4'>
                             <input type='hidden' name='session_destroy' value='1'>
                             <input class='btn btn-primary' type='submit' value='Se déconnecter'> 
                         </div>
                     </form>
                 </nav>
             </div>
+            
         ";
         }
         else {  
